b25836241a897c83298a1365745e424150ce9e27
Dhanveer007 <singhdhanveer007@gmail.com>
2021-02-25 17:59:59 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var a = 5;
-var b = 10;
-var c = a + b;
-document.write(c);
+var rows, cols;
+var n = 5;
+for (rows = 1; rows <= n; roww++) {
+  for (cols = 1; cols <= rows; cols++) {
+    document.write("* ");
+  }
+  document.write("<br>");
+}
